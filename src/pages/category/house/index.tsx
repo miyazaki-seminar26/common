897ca506b4e3c2793fe664e5a1edd501df46d71f
tokumoto-jin <cgeg0826@mail3.doshisha.ac.jp>
2023-02-15 15:47:23 +0900

    feat: 食、住、公共福祉のページ作りました。

--- src/pages/category/house/index.tsx
+++ src/pages/category/house/index.tsx
@@ -1,13 +1,32 @@
-import { Text } from '@mantine/core';
+import { Box, Center, Container, Image, Text } from '@mantine/core';
 import type { NextPage } from 'next';
 
 import { AppLayoutWithHeader } from '@/components/layouts/AppLayoutWithHeader';
+import { House } from '@/components/pages/category/house';
 
-const House: NextPage = () => {
+const HousePage: NextPage = () => {
   return (
     <AppLayoutWithHeader>
-      <Text>Play</Text>
+      <Center className="relative mx-2 h-[40vh]">
+        <Box className="absolute bottom-0">
+          <Image
+            src="/images/category/house/top.svg"
+            alt=""
+            w={'100vw'}
+            className="border-b opacity-80"
+          />
+        </Box>
+        <Text weight={800} size="lg" className="z-10 font-sans">
+          社会活動において
+          <br />
+          それって当たり前ですか？
+        </Text>
+      </Center>
+
+      <Container mx={{ md: 'auto', xs: 0 }}>
+        <House />
+      </Container>
     </AppLayoutWithHeader>
   );
 };
-export default House;
+export default HousePage;
