e8cce8b0a6a39959deb058c3a3c24e5287dca9d2
Shin.Yamamoto <yamashin0123@outlook.jp>
2023-02-15 15:28:13 +0900

    feat: add svg file

--- src/pages/category/clothes/index.tsx
+++ src/pages/category/clothes/index.tsx
@@ -1,4 +1,4 @@
-import { Center, Text } from '@mantine/core';
+import { Box, Center, Container, Image, Text } from '@mantine/core';
 import type { NextPage } from 'next';
 
 import { AppLayoutWithHeader } from '@/components/layouts/AppLayoutWithHeader';
@@ -6,15 +6,23 @@ import { AppLayoutWithHeader } from '@/components/layouts/AppLayoutWithHeader';
 const Clothes: NextPage = () => {
   return (
     <AppLayoutWithHeader>
-      <div className="min-h-max">
-        背景に動く系の画像を入れる
-        <Center className="h-[98vh]">
-          <Text weight={800} size="xl" className="font-sans">
-            それって当たり前ですか？
-          </Text>
-        </Center>
-      </div>
-      <Text>Clothes</Text>
+      <Center className="relative mx-2 h-[40vh]">
+        <Box className="absolute bottom-0">
+          <Image
+            src="/images/category/clothes/top.svg"
+            alt=""
+            w={'100vw'}
+            className="border-b opacity-80"
+          />
+        </Box>
+        <Text weight={800} size="lg" className="z-10 font-sans">
+          衣類において
+          <br />
+          それって当たり前ですか？
+        </Text>
+      </Center>
+
+      <Container mx={{ md: 'auto', xs: 0 }}></Container>
     </AppLayoutWithHeader>
   );
 };
